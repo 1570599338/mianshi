c7443b63cd3b7627207ee1caec2382c366328e43
1570599338@qq.com <1570599338@qq.com>
2020-04-02 01:42:45 +0800

    aqs算法

--- src/main/java/com/lquan/jdk/singelton/TestSingleton.java
+++ src/main/java/com/lquan/jdk/singelton/TestSingleton.java
@@ -1,6 +1,8 @@
 package com.lquan.jdk.singelton;
 
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicStampedReference;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
 
 public class TestSingleton {
 
@@ -10,6 +12,15 @@ public class TestSingleton {
 		
 		AtomicInteger a = new AtomicInteger(0);
 		a.incrementAndGet();
+		
+		AtomicStampedReference  aa = new AtomicStampedReference(100, 1);
+		System.out.println(aa.compareAndSet(100, 100, aa.getStamp(), aa.getStamp()));
+		
+		System.out.println(aa.getReference()+"*********"+aa.getStamp());
+		
+		
+		// ================== AQS =================
+		AbstractQueuedSynchronizer
 
 	}
 
